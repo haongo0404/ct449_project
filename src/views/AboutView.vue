0e409783c51741ab4bb6f025a60688e39561a3a4
haongo0404 <ngohao04042016@gmail.com>
2023-05-06 03:28:37 +0700

    frontend

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
-    <h1>Homeabout</h1>
+    <h1></h1>
+    <h3>email:Haob1910372@studen.ctu.edu.vn</h3>
+    <img src="https://goo.gl/maps/81okB7SecoCGKh3b7" alt="">
     <TheWelcome />
   </main>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,17 @@
   <main>
     <h1>Home</h1>
     <TheWelcome />
+    <div class="container d-flex justify-content-evenly">
+      <div class="row col-4">
+        <img src="https://vcdn-vnexpress.vnecdn.net/2020/09/08/hs-tieu-hoc-4175-1599563722.jpg" alt="">
+      </div>
+      <div class="row col-4">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ8E1Z8DQm9VFKdeLWHb3ViwGBxK7BS7veZrVp-hZ8&s" alt="">
+      </div>
+      <div class="row col-4">
+        <img src="https://nld.mediacdn.vn/2019/9/5/69667313550386679032402346238775682138112n-15676478388471893036956.jpg" alt="">
+      </div>
+    </div>
+    <video src="https://www.youtube.com/watch?v=qfHzdKql11w" class="object-fit-contain" autoplay></video>
   </main>
 </template>
